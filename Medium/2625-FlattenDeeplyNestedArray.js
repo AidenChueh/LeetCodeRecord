77a9ef3b390047b00995c67807aed2c8e5cbf1a8
aiden1223 <aidenchiueh@gmail.com>
2025-01-12 22:44:58 +0800

    completed

---
+++ Medium/2625-FlattenDeeplyNestedArray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    if (n === 0) {
+        return arr;
+    }
+
+    const flatArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            flatArr.push(...flat(arr[i], n - 1));
+        } else {
+            flatArr.push(arr[i]);
+        }
+    }
+
+    return flatArr;
+};
