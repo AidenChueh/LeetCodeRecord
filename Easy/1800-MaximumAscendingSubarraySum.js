c9fb60705a2ca79e0424b12de6823ad43b3e5d69
aiden1223 <aidenchiueh@gmail.com>
2025-02-04 17:27:34 +0800

    done

--- Easy/1800-MaximumAscendingSubarraySum.js
+++ Easy/1800-MaximumAscendingSubarraySum.js
@@ -2,6 +2,26 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxAscendingSum = function(nums) {
-    
+var maxAscendingSum = function (nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let max = nums[0];
+    let cur = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        let x = nums[i - 1];
+        let y = nums[i];
+
+        if (x >= y) {
+            cur = 0;
+            continue;
+        }
+
+        cur = (cur == 0) ? x + y : cur + y;
+        max = Math.max(max, cur);
+    }
+
+    return max;
 };
