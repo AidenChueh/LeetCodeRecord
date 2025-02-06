799a6900a346f732a0b57eb7a70b62e07cbdf17a
aiden1223 <aidenchiueh@gmail.com>
2025-02-06 11:56:36 +0800

    done

--- Medium/1726-TupleWithSameProduct.js
+++ Medium/1726-TupleWithSameProduct.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var tupleSameProduct = function (nums) {
+    let map = new Map();
+    let result = 0;
 
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let product = nums[i] * nums[j];
+            map.set(product, (map.get(product) || 0) + 1);
+        }
+    }
+
+    for (let product of map.values()) {
+        if (product >= 2) {
+            result += (product * (product - 1) / 2) * 8;
+        }
+    }
+
+    return result;
 };
