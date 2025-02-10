f1d9fd33efd5332063002fd2b44970c6368cf29b
aiden1223 <aidenchiueh@gmail.com>
2025-02-10 16:20:25 +0800

    done

--- Easy/3174-ClearDigits.js
+++ Easy/3174-ClearDigits.js
@@ -3,5 +3,15 @@
  * @return {string}
  */
 var clearDigits = function (s) {
+    let stack = [];
+    for (let i of s) {
+        if (!isNaN(i)) {
+            if (stack.length > 0) stack.pop();
+        }
+        else {
+            stack.push(i);
+        }
+    }
 
+    return stack.join('');
 };
