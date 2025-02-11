76b204844dfa251b91b37d79264cd9f64bc6872c
aiden1223 <aidenchiueh@gmail.com>
2025-02-11 10:16:31 +0800

    done

--- Medium/1910-RemoveAllOccurrencesOfASubstring.js
+++ Medium/1910-RemoveAllOccurrencesOfASubstring.js
@@ -4,5 +4,12 @@
  * @return {string}
  */
 var removeOccurrences = function (s, part) {
+    let idx = s.indexOf(part);
 
+    while (idx !== -1) {
+        s = s.substring(0, idx) + s.substring(idx + part.length);
+        idx = s.indexOf(part);
+    }
+
+    return s;
 };
