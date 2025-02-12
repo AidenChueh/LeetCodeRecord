9756e1366b361f65e2a41b3bea5fa4feb51a652d
aiden1223 <aidenchiueh@gmail.com>
2025-02-12 10:44:25 +0800

    done

--- Medium/2342-MaxSumOfAPairWithEqualSumOfDigits.js
+++ Medium/2342-MaxSumOfAPairWithEqualSumOfDigits.js
@@ -3,5 +3,21 @@
  * @return {number}
  */
 var maximumSum = function (nums) {
+    const sumNum = (num) => num.toString().split('').reduce((sum, n) => sum += parseInt(n), 0);
 
+    let sumMap = new Map();
+    let maxSum = -1;
+
+    for (let num of nums) {
+        const sum = sumNum(num);
+
+        if (sumMap.has(sum)) {
+            maxSum = Math.max(maxSum, num + sumMap.get(sum));
+            sumMap.set(sum, Math.max(num, sumMap.get(sum)));
+        } else {
+            sumMap.set(sum, num);
+        }
+    }
+
+    return maxSum;
 };
