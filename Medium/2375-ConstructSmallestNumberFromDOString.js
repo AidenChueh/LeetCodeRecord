4b7c6b35f02dae63b0ba29c6f1f7a94961875fc9
aiden1223 <aidenchiueh@gmail.com>
2025-02-18 11:12:25 +0800

    done

--- Medium/2375-ConstructSmallestNumberFromDOString.js
+++ Medium/2375-ConstructSmallestNumberFromDOString.js
@@ -3,5 +3,17 @@
  * @return {string}
  */
 var smallestNumber = function (pattern) {
+    const n = pattern.length;
+    const ans = [];
+    let stack = [];
 
+    for (let i = 0; i <= n; i++) {
+        stack.push(i + 1);
+        if (pattern[i] === "I" || i === n) {
+            ans.push(...stack.reverse());
+            stack = [];
+        }
+    }
+
+    return ans.join[''];
 };
